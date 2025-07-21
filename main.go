aeb2c63d24094ef4b2c4273eaad067b067545386
Waterbootdev <wassermann.sp@gmail.com>
2025-07-21 21:10:37 +0200

    feat: reorganisiere Hauptfunktion und strukturiere HTTP-Handler für bessere Lesbarkeit

--- main.go
+++ main.go
@@ -9,27 +9,42 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	godotenv.Load()
-
-	apiCfg := apiconfig.NewApiConfig()
-
-	serveMux := http.NewServeMux()
-
-	httpServer := http.Server{
-		Addr:    ":8080",
-		Handler: serveMux,
-	}
+func setFileServerHandle(serveMux *http.ServeMux, apiCfg *apiconfig.ApiConfig) {
 
 	httpFileServer := apiCfg.MiddlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 
 	serveMux.Handle("/app/", httpFileServer)
 	serveMux.Handle("/app/assets", httpFileServer)
+}
 
-	serveMux.HandleFunc("GET /api/healthz", healthzHandler)
+func setAdminHandleFuncs(serveMux *http.ServeMux, apiCfg *apiconfig.ApiConfig) {
 	serveMux.HandleFunc("GET /admin/metrics", apiCfg.MetricsHandler)
 	serveMux.HandleFunc("POST /admin/reset", apiCfg.ResetHandle)
-	serveMux.HandleFunc("POST /api/validate_chirp", validateChirpLengthAndCleanProfaneWords)
 
-	httpServer.ListenAndServe()
+}
+
+func setApiHandleFuncs(serveMux *http.ServeMux) {
+	serveMux.HandleFunc("GET /api/healthz", healthzHandler)
+	serveMux.HandleFunc("POST /api/validate_chirp", validateChirpLengthAndCleanProfaneWords)
+}
+
+func newServeMux(apiCfg *apiconfig.ApiConfig) *http.ServeMux {
+	serveMux := http.NewServeMux()
+	setFileServerHandle(serveMux, apiCfg)
+	setAdminHandleFuncs(serveMux, apiCfg)
+	setApiHandleFuncs(serveMux)
+	return serveMux
+}
+
+func httpServer(apiCfg *apiconfig.ApiConfig) (httpServer *http.Server) {
+	return &http.Server{
+		Addr:    ":8080",
+		Handler: newServeMux(apiCfg),
+	}
+}
+
+func main() {
+	godotenv.Load()
+
+	httpServer(apiconfig.NewApiConfig()).ListenAndServe()
 }
